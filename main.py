9d9dfbd3c2b2245002ab5da965c078c838d5077e
Иван Тохтунов <itokhtunov@yandex.ru>
2023-02-07 22:16:06 +0300

    Annotation to variables and functions

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику'
-            f' равный {5 + randint(3, 5)}'
-            )
+            f' равный {5 + randint(3, 5)}')
     if char_class == 'mage':
         return (
             f'{char_name} нанёс урон противнику '
-            f'равный {5 + randint(5, 10)}'
-            )
+            f'равный {5 + randint(5, 10)}')
     if char_class == 'healer':
         return (
             f'{char_name} нанёс урон противнику '
-            f'равный {5 + randint(-3, -1)}'
-            )
+            f'равный {5 + randint(-3, -1)}')
     return ('Такого персонажа нет')
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -30,11 +27,11 @@ def defence(char_name, char_class):
     return ('Такого персонажа нет')
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (
-            f'{char_name} применил специальное умение «Выносливость {80 + 25}»'
-            )
+            f'{char_name} применил специальное '
+            f'умение «Выносливость {80 + 25}»')
     if char_class == 'mage':
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
@@ -42,7 +39,7 @@ def special(char_name, char_class):
     return ('Такого персонажа нет')
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -53,10 +50,9 @@ def start_training(char_name, char_class):
     print(
         'Введи одну из команд: attack — чтобы атаковать противника, defence '
         '— чтобы блокировать атаку противника или special — чтобы '
-        'использовать свою суперсилу.'
-        )
+        'использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = None
+    cmd: str = ''
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -68,33 +64,28 @@ def start_training(char_name, char_class):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class = input(
             'Введи название персонажа, за которого хочешь играть: '
-            'Воитель — warrior, Маг — mage, Лекарь — healer: '
-            )
+            'Воитель — warrior, Маг — mage, Лекарь — healer: ')
         if char_class == 'warrior':
             print(
                 'Воитель — дерзкий воин ближнего боя. Сильный, '
-                'выносливый и отважный.'
-                )
+                'выносливый и отважный.')
         if char_class == 'mage':
             print(
                 'Маг — находчивый воин дальнего боя. '
-                'Обладает высоким интеллектом.'
-                )
+                'Обладает высоким интеллектом.')
         if char_class == 'healer':
             print(
                 'Лекарь — могущественный заклинатель. '
-                'Черпает силы из природы, веры и духов.'
-                )
+                'Черпает силы из природы, веры и духов.')
         approve_choice = input(
             'Нажми (Y), чтобы подтвердить выбор, или любую другую кнопку, '
-            'чтобы выбрать другого персонажа '
-            ).lower()
+            'чтобы выбрать другого персонажа ').lower()
     return char_class
 
 
